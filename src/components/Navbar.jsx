de82a32576191f99d1112700db406c5b4f3e1116
biplovsoftwarica <240148@softwarica.edu.np>
2024-11-26 18:23:28 +0545

    made navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,17 +13,22 @@ const NavbarMenu = [
   },
   {
     id: 2,
-    title: "Categories",
+    title: "Products",
     link: "#",
   },
   {
     id: 3,
-    title: "Blog",
+    title: "Contact",
     link: "#",
   },
   {
     id: 4,
     title: "About",
+    link: "#",
+  },
+  {
+    id: 5,
+    title: "Blogs",
     link: "#",
   },
 ];
@@ -36,35 +41,40 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="sticky top-0 z-50 flex items-center justify-between p-4 bg-transparent text-white">
-      <div className="flex items-center">
-        <Link to="/" className="transition duration-300 ease-in-out transform hover:scale-105 drop-shadow-md hover:drop-shadow-lg">
-          <img className="brand" src={Logo} alt="Logo" style={{ height: '75px' }} />
-        </Link>
-      </div>
-      <div className={`nav-links ${menuOpen ? 'top-16 opacity-100' : 'top-[-400px] opacity-0'} absolute left-0 w-full bg-transparent md:static md:w-auto md:opacity-100 md:flex md:items-center ml-auto pr-4 transition-all duration-300 ease-in-out`}>
-        <ul className="flex flex-col md:flex-row gap-2.5">
-          {NavbarMenu.map((item) => (
-            <li key={item.id} className="p-2">
-              <a href={item.link} className="inline-block text-base font-semibold py-2 px-3 uppercase hover:text-black transition duration-300 ease-in-out transform hover:scale-110 text-shadow hover:text-shadow-white">
-                {item.title}
-              </a>
+    <>
+      {/* Full-page overlay for mobile view */}
+      <div className={`fixed inset-0 md:hidden ${menuOpen ? 'bg-black/10 backdrop-blur-md' : 'bg-transparent'} transition-all duration-300 ease-in-out z-40`} onClick={toggleMenu}></div>
+      
+      <nav className="sticky top-0 z-50 flex items-center justify-between p-4 bg-transparent text-white">
+        <div className="flex items-center justify-center">
+          <Link to="/" className="transition duration-300 ease-in-out transform hover:scale-105 drop-shadow-md hover:drop-shadow-lg">
+            <img className={`brand ml-2 ${menuOpen ? 'blur-md' : ''}`} src={Logo} alt="Logo" style={{ height: '75px' }} />
+          </Link>
+        </div>
+        <div className={`nav-links ${menuOpen ? 'top-16 opacity-100' : 'top-[-400px] opacity-0'} absolute left-0 w-full bg-transparent md:static md:w-auto md:opacity-100 md:flex md:items-center ml-auto pr-4 transition-all duration-300 ease-in-out`}>
+          <ul className={`flex flex-col md:flex-row gap-2.5 ${menuOpen ? 'items-center' : 'items-center'}`}>
+            {NavbarMenu.map((item) => (
+              <li key={item.id} className="p-2">
+                <a href={item.link} className="inline-block text-base font-semibold py-2 px-3 uppercase hover:text-black transition duration-300 ease-in-out transform hover:scale-110 text-shadow hover:text-shadow-white">
+                  {item.title}
+                </a>
+              </li>
+            ))}
+            <li className="p-2 flex items-center space-x-4 w-32 justify-evenly">
+              <button className="text-xl hover:text-black transition duration-300 ease-in-out text-shadow hover:text-shadow-lg">
+                <FaShoppingCart className="drop-shadow-custom-lg hover:drop-shadow-custom-xl transform hover:scale-110" />
+              </button>
+              <button className="text-xl hover:text-black transition duration-300 ease-in-out text-shadow hover:text-shadow-lg mt-1">
+                <FaRegUser className="drop-shadow-md hover:drop-shadow-lg transform hover:scale-110" />
+              </button>
             </li>
-          ))}
-          <li className="p-2 flex items-center space-x-4 w-32 justify-evenly">
-            <button className="text-xl hover:text-black transition duration-300 ease-in-out text-shadow hover:text-shadow-lg">
-              <FaShoppingCart className="drop-shadow-custom-lg hover:drop-shadow-custom-xl transform hover:scale-110" />
-            </button>
-            <button className="text-xl hover:text-black transition duration-300 ease-in-out text-shadow hover:text-shadow-lg mt-1">
-              <FaRegUser className="drop-shadow-md hover:drop-shadow-lg transform hover:scale-110" />
-            </button>
-          </li>
-        </ul>
-      </div>
-      <div className="md:hidden ml-auto">
-        <MdMenu className="text-4xl cursor-pointer" onClick={toggleMenu} />
-      </div>
-    </nav>
+          </ul>
+        </div>
+        <div className="md:hidden ml-auto z-50">
+          <MdMenu className="text-4xl cursor-pointer" onClick={toggleMenu} />
+        </div>
+      </nav>
+    </>
   );
 };
 
