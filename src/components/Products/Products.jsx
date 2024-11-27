de7dd194f4959799b96f3e36d20bc57a250d4449
biplovsoftwarica <240148@softwarica.edu.np>
2024-11-27 22:47:37 +0545

    navbar scrool smoothness

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -86,19 +86,19 @@ const Products = () => {
           OUR PRODUCTS
         </motion.h1>
         {/* card section */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-4 gap-6">
         {ProductsData.map((item) => (
           <Link to={`/product/${item.id}`} key={item.id} className="group">
           <motion.div
             variants={fadeUp(0.5)}
             initial="hidden"
             whileInView={"show"}
-            className="flex flex-col items-center justify-center p-5 w-[300px] mx-auto shadow-lg rounded-xl bg-customorangelight2 h-[380px] transform transition duration-300 group-hover:scale-120 group-hover:bg-customorangelight"
+            className="flex flex-col items-center justify-center p-5 max-w-[300px] mx-auto shadow-lg rounded-xl bg-customorangelight2 h-[380px] transform transition duration-300 group-hover:scale-105 group-hover:bg-customorangelight"
           >
             <img
               src={item.image}
               alt=""
-              className="w-[100px] mb-4 group-hover:rotate-12 group-hover:scale-120 duration-300"
+              className="w-[100px] mb-4 group-hover:rotate-12 group-hover:scale-110 duration-300"
             />
             <div className="text-center space-y-2">
               <h1 className="text-2xl text-white font-semibold text-center">
